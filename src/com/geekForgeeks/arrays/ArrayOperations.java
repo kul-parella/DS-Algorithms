95138f60f30f14270371603039b36fa565792733
kul-parella <kul.parella@gmail.com>
2020-11-28 04:04:23 -0500

    updated array operations

--- src/com/geekForgeeks/arrays/ArrayOperations.java
+++ src/com/geekForgeeks/arrays/ArrayOperations.java
@@ -3,14 +3,14 @@ package com.geekForgeeks.arrays;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-public class InsertElementArray {
+public class ArrayOperations {
 
     public static void main(String[] args) {
 
         int[] arr = new int[8];
         arr[0]=4;
         arr[1]=0;
-        arr[2]=5;
+        arr[2]=9;
         arr[3]=6;
         arr[4]=0;
         arr[5]=7;
@@ -25,7 +25,10 @@ public class InsertElementArray {
         //System.out.println("Array sorted::"+arraySorted(arr));
        // Arrays.stream(reverseArray(arr)).forEach(x-> System.out.println(x));
         //System.out.println("new Size::"+removeDuplicatesFromSortedArr(arr, 8));
-        Arrays.stream(moveZerosToEndWithoutOrderChange(arr)).forEach(x-> System.out.println(x));
+        //Arrays.stream(moveZerosToEndWithoutOrderChange(arr)).forEach(x-> System.out.println(x));
+        //Arrays.stream(leftRotateArray_D_Positions(arr, 2)).forEach(x->System.out.println(x));
+        //Arrays.stream(leftRotateArray_D_Positions_better(arr, 2)).forEach(x->System.out.println(x));
+        printLeadersOfArray(arr);
     }
 
     public static int[] insert(int[] arr, int offsetToInsert, int elementToInsert) {
@@ -161,10 +164,75 @@ public class InsertElementArray {
         return newArr;
     }
 
-//    public static int[] leftRotateArray_D_Positions(int[] arr, int position) {
+    public static int[] leftRotateArray_D_Positions(int[] arr, int position) {
+
+        int[] newArr = new int[position];
+
+        //copying the d positions into temp array
+        for(int i=0; i<position-1; i++) {
+            newArr[i] = arr[i];
+        }
+
+        for(int i=position, j=0; i<arr.length;i++, j++) {
+            arr[j] = arr[i];
+        }
+
+        for(int i = arr.length-1, j=newArr.length-1; j>=0 ; j--, i--) {
+            arr[i] = newArr[j];
+        }
+
+    return arr;
+
+    }
+
+    public static int[] leftRotateArray_D_Positions_better(int[] arr, int position) {
+
+        //creating new array with same length as original array
+        int[] positionReverseArr = new int[arr.length];
+
+        //  reverse original array elements still position and save it to new array
+        // original array [4 0 5 6 0 7 0 8], say position is 2
+        // new array will be [0 4 0 0 0 0 0 0]
+        for (int i = position - 1, j = 0; i >= 0; i--, j++) {
+            positionReverseArr[j] = arr[i];
+        }
+
+        //reverse the elements in the second half, position to end of the array and save it to the end of the new array
+        // new array will be [0 4 8 0 7 0 6 5]
+        for (int i = arr.length - 1, j = position; i >= position; i--, j++) {
+            positionReverseArr[j] = arr[i];
+        }
+
+        //reverse the new array and save it to original array and return
+        //[5 6 0 7 0 8 4 0]
+        for (int i = positionReverseArr.length - 1, j = 0; i >= 0; i--, j++) {
+            arr[j] = positionReverseArr[i];
+        }
+
+        return arr;
+    }
+
+
+    // leaders in array is something, number which doesn't have greater values in the right side of it.
+    public static void printLeadersOfArray(int[] arr) {
+
+      int presentLeader = arr[arr.length-1];
+
+      System.out.print(presentLeader);
+
+      for(int i=arr.length-2; i>=0; i--) {
+          if(arr[i]>presentLeader) {
+              System.out.print(" "+arr[i]);
+          }
+      }
+
+    }
+
+//    public static void maxDifference(int[] arr) {
+//
+//
+//        for()
 //
 //    }
-
-
 
 }
